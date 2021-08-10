be4c4994027a1c5f0ce75468e4651949ddcb737c
Paulo <pauloeduardo2709@gmail.com>
2021-08-10 00:51:09 -0300

    test: add simple len based test

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/Pauloo27/searchtube
 
 go 1.16
 
-require github.com/buger/jsonparser v1.1.1
+require (
+	github.com/buger/jsonparser v1.1.1
+	github.com/stretchr/testify v1.7.0
+)
---
+++ main_test.go
@@ -0,0 +1,28 @@
+package search
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSearchtube(t *testing.T) {
+	t.Run("Test search 1", func(t *testing.T) {
+		results, err := Search("free software song", 1)
+		assert.Nil(t, err)
+		assert.NotNil(t, results)
+		assert.Equal(t, len(results), 1)
+	})
+	t.Run("Test search 10", func(t *testing.T) {
+		results, err := Search("free software song", 10)
+		assert.Nil(t, err)
+		assert.NotNil(t, results)
+		assert.Equal(t, len(results), 10)
+	})
+	t.Run("Test search all", func(t *testing.T) {
+		results, err := Search("free software song", -1)
+		assert.Nil(t, err)
+		assert.NotNil(t, results)
+		assert.GreaterOrEqual(t, len(results), 1)
+	})
+}

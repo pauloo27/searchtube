1828077b9033019ea3c920d3a7e319c1aab2cd6c
Paulo <git@pauloo27.dev>
2021-12-10 18:31:29 -0300

    feat: add user agent

--- main.go
+++ main.go
@@ -52,6 +52,7 @@ func Search(searchTerm string, limit int) (results []*SearchResult, err error) {
 		return nil, fmt.Errorf("Cannot create GET request: %v", err)
 	}
 	req.Header.Add("Accept-Language", "en")
+	req.Header.Add("User-Agent", "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36")
 
 	res, err := httpClient.Do(req)
 	if err != nil {
